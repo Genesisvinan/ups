cbd1e6f177e25bd4660c239810d275bf2a0f4f84
Genesisvinan <gvinan@est.ups.edu.ec>
2025-01-24 13:10:51 -0500

    Add(I): Add packages Person, PersonController and Perservice

--- src/main/java/com/example/ups/poo/controllers/TestController.java
+++ src/main/java/com/example/ups/poo/controllers/TestController.java
@@ -8,19 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "Hello world, this is my first StringBoot Project...!";
     }
 
-    @GetMapping ("/greet/{name}/{lastname}")
-    public String greet(@PathVariable String name, @PathVariable String lastname){
-        String message ="Hello " + name + " " + lastname + ", this is my first StringBoot Project...!";
+    @GetMapping("/greet/{name}/{lastname}")
+    public String greet(@PathVariable String name, @PathVariable String lastname) {
+        String message = "Hello " + name + " " + lastname + ", this is my first StringBoot Project...!";
         return message;
     }
-    
+
     @GetMapping("/hello")
-    public String hello (@RequestParam String name, @RequestParam String lastname){
-        String message ="Hello " + name + " " + lastname + ", this is my first StringBoot Project...!";
-        return message;
+    public String hello(@RequestParam String name, @RequestParam(required = false) String lastname) {
+        if (lastname == null) {
+            String message = "Hello " + name +", this is my first StringBoot Project...!";
+            return message;
+        } else {
+            String message = "Hello " + name + " " + lastname + ", this is my first StringBoot Project...!";
+            return message;
+        }
     }
 }
---
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -0,0 +1,19 @@
+package com.example.ups.poo.service;
+
+import com.example.ups.poo.dto.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonService {
+    public List<Person> getAllPeople(){
+        List<Person>personList = new ArrayList<>();
+        Person p1 = new Person("genesis", "vinan", 21, "0950368654");
+        Person p2 = new Person("diego", "vinan", 24, "0950368688");
+        personList.add(p1);
+        personList.add(p2);
+        return personList;
+    }
+}
