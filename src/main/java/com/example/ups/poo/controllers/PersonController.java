1b60767b0389506b4aff2b90da7264a3ac5e1664
Genesisvinan <gvinan@est.ups.edu.ec>
2025-01-31 12:59:17 -0500

    Add(I): Add Update Person Controller

--- src/main/java/com/example/ups/poo/controllers/PersonController.java
+++ src/main/java/com/example/ups/poo/controllers/PersonController.java
@@ -31,5 +31,11 @@ public class PersonController{
     public ResponseEntity createPerson(@RequestBody Person person){
         return personService.createPerson(person);
     }
+
+    @PutMapping("/update-person")
+    public ResponseEntity updatePerson(@RequestBody Person person){
+        return personService.updatePerson(person);
+    }
+
 }
 
