bb3773baa3e46a89dd8a9f5b81624475eeb59ea7
Genesisvinan <gvinan@est.ups.edu.ec>
2025-02-04 08:24:32 -0500

    Add(I): Add Delete Person controller

--- src/main/java/com/example/ups/poo/service/PersonService.java
+++ src/main/java/com/example/ups/poo/service/PersonService.java
@@ -1,6 +1,7 @@
 package com.example.ups.poo.service;
 
 import com.example.ups.poo.dto.Person;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -21,47 +22,63 @@ public class PersonService {
     }
 
     // TODO: Create method that finds and returns person by id.
-    public ResponseEntity getPeronById(String id){
-        for(Person person: personList){
-            if (id.equalsIgnoreCase(person.getId())){
+    public ResponseEntity getPeronById(String id) {
+        for (Person person : personList) {
+            if (id.equalsIgnoreCase(person.getId())) {
                 return ResponseEntity.status(HttpStatus.OK).body(person);
             }
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id: " + id + " not found");
     }
 
-    public ResponseEntity createPerson(Person person){
+    public ResponseEntity createPerson(Person person) {
         for (Person registeredPerson : personList) {
             if (registeredPerson.getId().equals(person.getId())) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The ID is already registered");
             }
         }
-        if (person.getName() == null || person.getName().isEmpty() || person.getName().isBlank()){
+        if (person.getName() == null || person.getName().isEmpty() || person.getName().isBlank()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Name is required");
-        }else if (person.getLastname() == null || person.getLastname().isEmpty() || person.getLastname().isBlank()) {
+        } else if (person.getLastname() == null || person.getLastname().isEmpty() || person.getLastname().isBlank()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Lastname is required");
-        }else if (person.getAge() <= 0) {
+        } else if (person.getAge() <= 0) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Age is required");
-        }else if (person.getId() == null || person.getId().isEmpty() || person.getId().isBlank()){
+        } else if (person.getId() == null || person.getId().isEmpty() || person.getId().isBlank()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Id is required");
         }
 
         personList.add(person);
         return ResponseEntity.status(HttpStatus.OK).body("Person successfully registered");
     }
-    public ResponseEntity updatePerson(Person person){
-        for(Person per: personList){
-            if(per.getId().equalsIgnoreCase(person.getId())) {
-                if (person.getName() != null ) {
+
+    public ResponseEntity updatePerson(Person person) {
+        for (Person per : personList) {
+            if (per.getId().equalsIgnoreCase(person.getId())) {
+                if (person.getName() != null) {
                     per.setName(person.getName());
-                }if (person.getLastname() != null ) {
+                }
+                if (person.getLastname() != null) {
                     per.setLastname(person.getLastname());
-                }if (person.getAge() > 0){
+                }
+                if (person.getAge() > 0) {
                     per.setAge(person.getAge());
                 }
-            return ResponseEntity.status(HttpStatus.OK).body("Person with id: " + person.getId() + " was successfully updated");
+                return ResponseEntity.status(HttpStatus.OK).body("Person with id: " + person.getId() + " was successfully updated");
             }
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id: " + person.getId() + " not found");
+    }
+
+    public ResponseEntity deletePersonById(String id) {
+        if (id != null && id.length() < 10) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("id: " + id + "does not have the required length (10 chars min");
+        }
+        for (Person person : personList) {
+            if (id.equalsIgnoreCase(person.getId())) {
+                personList.remove(person);
+                return ResponseEntity.status(HttpStatus.OK).body("Person with id: " + id + " was succesfully deleted");
+            }
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Person with id: " + id + "was not found");
     }
 }
